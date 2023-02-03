82d9c8eef608e7d8f0ca487b7531c588f4fb689a
Andrew Zaiko <andrewzaiko@gmail.com>
2023-02-03 19:11:36 +0300

    Настроил таблицу пользователей в панели администратора

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
 from .models import User
 
-admin.site.register(User, UserAdmin)
 
+@admin.register(User)  # Регистрируем и настраиваем таблицу пользователей в панели администратора
+class UserAdmin(admin.ModelAdmin):
+    list_display = ["username", "email", "first_name", "last_name"]  # поля списка пользователей
+    exclude = ["password"]  # не показывать пароль пользователя
+    readonly_fields = ["date_joined", "last_login"]  # поля только для чтения
+    search_fields = ["username", "email", "first_name", "last_name"]  # поиск по полям
+    search_help_text = "Search by username, email, first_name or last_name"  # подсказка поиска
+    list_filter = ["is_staff", "is_active", "is_superuser"]  # фильтр списка пользователей
